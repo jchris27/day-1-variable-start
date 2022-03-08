5a0853f9b5f001548421670d7a9a49ac999b152a
Jin <53550425+jchris27@users.noreply.github.com>
2022-03-08 07:37:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# name = input("What is your name? ")
+# print(name)
+# print("Hello" + " " + name)
+
+# switching numbers
+a = input("A: ")
+b = input("B: ")
+
+c = a
+a = b
+b = c
+
+print("A: " + a)
+print("B " + b)
